fa246ea0647c1c485a6f4e0ef2c3ac773df50625
Beatriz Mattos <venganzakills@gmail.com>
2020-09-24 00:34:48 -0300

    instalando dependências e implementando arquivos base

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import { AddressInfo } from "net";
+import express from "express";
+import dotenv from "dotenv";
+
+if (process.env.NODE_ENV !== "serverless") {
+    dotenv.config();
+};
+
+export const app = express();
+
+app.use(express.json());
+
+export default app;
+
+if (process.env.NODE_ENV !== "serverless") {
+    const server = app.listen(process.env.PORT || 3003, () => {
+        if (server) {
+            const address = server.address() as AddressInfo;
+            console.log(`Server is running in http://localhost:${address.port}`);
+        }
+        else {
+            console.error(`Failure upon starting server.`);
+        }
+    });
+};
