6bbd17497bb04b054498a2660b2bdf2a8929a73d
Beatriz Mattos <venganzakills@gmail.com>
2020-10-02 16:23:43 -0300

    removendo método serverless

--- src/local.ts
+++ src/local.ts
@@ -2,18 +2,14 @@ import { AddressInfo } from "net";
 import dotenv from "dotenv";
 import app from "./index";
 
-if (process.env.NODE_ENV !== "serverless") {
-    dotenv.config();
-};
+dotenv.config();
 
-if (process.env.NODE_ENV !== "serverless") {
-    const server = app.listen(process.env.PORT || 3003, () => {
-        if (server) {
-            const address = server.address() as AddressInfo;
-            console.log(`Server is running in http://localhost:${address.port}`);
-        }
-        else {
-            console.error(`Failure upon starting server.`);
-        }
-    });
-};
+const server = app.listen(process.env.PORT || 3003, () => {
+    if (server) {
+        const address = server.address() as AddressInfo;
+        console.info(`Server is running in http://localhost:${address.port}`);
+    }
+    else {
+        console.error(`Failure upon starting server.`);
+    }
+});
